da7921a32f71dc10f81131589bdec33e891551b2
srbellala <srbellala@gmail.com>
2024-10-19 17:20:46 -0500

    refresh

--- find-my-professor/src/App.js
+++ find-my-professor/src/App.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import { Router, Route, Routes, Link } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import ProfessorList from './ProfessorList';
-import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <header className="header">
-          <div className="logo">Find My Professor</div>
-          <nav>
-            <Link to="/">Professors</Link>
-            <Link to="/dashboard">Dashboard</Link>
-          </nav>
-        </header>
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<ProfessorList />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
